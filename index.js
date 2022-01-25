42e6e44f27b4f5ac146ce6e61e2dbf50b05a9f44
Jerry Zhang <jerryzhang101@yahoo.com>
2022-01-25 17:38:04 -0500

    Added code to create server; moved some files; everything works

---
+++ index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+http.createServer( (req, res) => {
+    let q = url.parse(req.url, true);
+    let fileName = "." + q.pathname;
+    fileName = fileName.toLowerCase();
+    if ( fileName === "./") {
+        fs.readFile("./index.html", (err, data) => {
+            if (err) {
+                res.writeHead(404, {"Content-Type": "text/html"});
+                return res.end("404 not found");
+            }
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(data);
+            return res.end();
+        })
+    }
+    else {
+        fs.readFile(fileName, (err, data) => {
+            if (err) {
+                res.writeHead(404, {"Content-Type": "text/html"});
+                return res.end("404 not found");
+            }
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(data);
+            return res.end();
+        })
+    }
+}).listen(8080);
